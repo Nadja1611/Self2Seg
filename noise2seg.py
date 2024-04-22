1643b8f342f40903cb105da04422a23bf0cb3098
Nadja1611 <61620034+Nadja1611@users.noreply.github.com>
2024-04-22 17:44:38 +0200

    Update noise2seg.py

--- noise2seg.py
+++ noise2seg.py
@@ -204,7 +204,7 @@ def noise2seg(image,
                     plt.imshow(torch.round(mynet.x.cpu()[0]))
                     plt.title('Final Segmentation')
                     plt.show()
-                    
+                    break
                     #plt.figure()
                     return mynet.x, mynet.f1
                     
